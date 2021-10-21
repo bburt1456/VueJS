c5e424b344ec50107c4b4e8a75d6700ba52b519f
bburt1456 <49314796+bburt1456@users.noreply.github.com>
2021-10-21 14:31:17 -0500

    Create app.js
    This is also part of Introduction, VueJS Basics 1, and VueJS Basics 2.

---
+++ app.js
@@ -0,0 +1,24 @@
+const app = Vue.createApp({
+    // data, functions
+    // template: '<h2>I am the template</h2>'
+    data(){
+        return {
+            showBooks: true,
+            title: 'The Final Empire',
+            author: 'Brandon Burt',
+            age: 20
+        }
+    },
+    methods: {
+      // changeTitle(title) {
+            //this.title = 'Words of Randiance'
+            //this.title = title
+      // } 
+      toggleShowBooks(){
+          this.showBooks = !this.showBooks
+      }
+    }
+})
+
+app.mount('#app')
+
