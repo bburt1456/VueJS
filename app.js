d3ded7f3e4f923523e539358f2d4a7f1935f4293
bburt42 <59900395+bburt42@users.noreply.github.com>
2021-10-26 13:39:24 -0500

    Outputting a list of elements in an array

--- app.js
+++ app.js
@@ -1,34 +1,17 @@
 const app = Vue.createApp({
-    // data, functions
-    // template: '<h2>I am the template</h2>'
     data(){
         return {
             showBooks: true,
-            title: 'The Final Empire',
-            author: 'Brandon Burt',
-            age: 20,
-            x:0,
-            y:0
+            books: [
+                { title: 'name of the wind', author: 'patrick mahomes'},
+                { title: 'the way of kings', author: 'brandon burt'},
+                { title: 'the final empire', author: 'brandon burt'},
+            ]
         }
     },
     methods: {
-      // changeTitle(title) {
-            //this.title = 'Words of Randiance'
-            //this.title = title
-      // } 
       toggleShowBooks(){
           this.showBooks = !this.showBooks
-      },
-      handleEvent(e, data){
-          console.log(e, e.type)
-          if(data){
-              console.log(data)
-          }
-      },
-      handleMousemove(e){
-        this.x = e.offsetX
-        this.y = e.offsetY
-
       }
     }
 })
